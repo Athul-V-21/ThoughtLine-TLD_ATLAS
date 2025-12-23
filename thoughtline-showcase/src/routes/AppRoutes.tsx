e5614d0af55f186713cb17621ff35caf9fb43278
J J Arya <jjarya2003@gmail.com>
2025-12-23 16:17:22 +0530

    AppRoutes commit

--- thoughtline-showcase/src/routes/AppRoutes.tsx
+++ thoughtline-showcase/src/routes/AppRoutes.tsx
@@ -1,22 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Splash from "../pages/Splash/Splash";
-import Login from "../pages/Auth/Login";
+import Login from "../pages/Auth/Login"; // ✅ Import Login
 import Signup from "../pages/Auth/Signup";
 import Home from "../pages/Home/Home";
+
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Splash />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Splash />} /> {/* Splash route */}
+        <Route path="/login" element={<Login />} /> {/* Login route */}
         <Route path="/signup" element={<Signup />} />
-
-        {/* ✅ ADD THIS */}
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<Home/>} />
       </Routes>
     </BrowserRouter>
   );
 };
 
 export default AppRoutes;
+
